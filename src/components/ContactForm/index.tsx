e2750b59238ae6c3c3dd7f30b6cdcc1d628c308e
Lucas Galvao <lucasmelo_galvao@hotmail.com>
2022-06-15 01:57:14 -0300

    changes

--- src/components/ContactForm/index.tsx
+++ src/components/ContactForm/index.tsx
@@ -1,4 +1,5 @@
 import { Box, Flex, HStack, Image, Text, VStack} from "@chakra-ui/react";
+import api from 'axios';
 import { useForm } from 'react-hook-form';
 import {
     FormErrorMessage,
@@ -8,105 +9,95 @@ import {
     Button,
   } from '@chakra-ui/react'
 import { useState } from "react";
+import { IForm } from "../../dtos/dtos";
 
 export function ContactForm() {
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [phone, setPhone] = useState('');
     const [subject, setSubject] = useState('');
+    const [formData, setFormData] = useState<IForm>();
 
     const {
         handleSubmit,
         register,
         formState: { errors, isSubmitting },
       } = useForm({mode: "all"});
+
     
-      async function submitForm() {
-        var formData = new FormData();
-
-        formData.append("name", name);
-        formData.append("email", email);
-        formData.append("phone", phone);
-        formData.append("subject", subject);
-
-        console.log(formData);
+      const onSubmit = (data: IForm) => {
+        api.post('localhost:3333/email', {
+          data,
+        })
       }
-    
+      
       return (
-        <form onSubmit={handleSubmit(submitForm)}>
-        <VStack spacing={4}>
-        <FormControl isInvalid={errors.name}>
-            <FormLabel htmlFor='name'>Nome</FormLabel>
-            <Input
-              id='name'
-              placeholder='Nome'
-              {...register('name', {
-                required: 'Campo Obrigatório',
-              })}
-              onChange={(event) => {
-                setName(event.target.value)
-            }}
-            />
-            <FormErrorMessage>
-              {errors.name}
-            </FormErrorMessage>
-          </FormControl>
-          
-          <FormControl isInvalid={errors.email}>
-            <FormLabel htmlFor='name'>Email</FormLabel>
-            <Input
-              id='email'
-              placeholder='Email'
-              {...register('email', {
-                required: 'This is required',
-              })}
-              onChange={(event) => {
-                setEmail(event.target.value)
-            }}
-            />
-            <FormErrorMessage>
-              {errors.email}
-            </FormErrorMessage>
-          </FormControl>
-          
-          <FormControl isInvalid={errors.email}>
-            <FormLabel htmlFor='name'>Telefone</FormLabel>
-            <Input
-              id='phone'
-              placeholder='Telefone'
-              {...register('phone', {
-                required: 'This is required',
-              })}
-              onChange={(event) => {
-                  setPhone(event.target.value)
-              }}
-            />
-            <FormErrorMessage>
-              {errors.email}
-            </FormErrorMessage>
-          </FormControl>
-          
-          <FormControl isInvalid={errors.email}>
-            <FormLabel htmlFor='name'>Assunto</FormLabel>
-            <Input
-              id='subject'
-              placeholder='Assunto'
-              {...register('subject', {
-                required: 'This is required',
-              })}
-              onChange={(event) => {
-                setSubject(event.target.value)
-            }}
-            />
-            <FormErrorMessage>
-              {errors.email}
-            </FormErrorMessage>
-          </FormControl>
-          
-          <Button mt={4} colorScheme='teal' isLoading={isSubmitting} type='submit'>
-            Enviar
-          </Button>
-        </VStack>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <VStack spacing={4}>
+          <FormControl isInvalid={errors.name}>
+              <FormLabel htmlFor='name'>Nome</FormLabel>
+              <Input
+                id='name'
+                placeholder='Nome'
+                {...register("name")}
+                onChange={(event) => {
+                  setName(event.target.value)
+                }}
+              />
+              <FormErrorMessage>
+                {errors.name}
+              </FormErrorMessage>
+            </FormControl>
+            
+            <FormControl isInvalid={errors.email}>
+              <FormLabel htmlFor='email'>Email</FormLabel>
+              <Input
+                id='email'
+                placeholder='Email'
+                {...register("email")}
+                onChange={(event) => {
+                  setEmail(event.target.value)
+                }}
+              />
+              <FormErrorMessage>
+                {errors.email}
+              </FormErrorMessage>
+            </FormControl>
+            
+            <FormControl isInvalid={errors.phone}>
+              <FormLabel htmlFor='phone'>Telefone</FormLabel>
+              <Input
+                id='phone'
+                placeholder='Telefone'
+                {...register("phone")}
+                onChange={(event) => {
+                    setPhone(event.target.value)
+                }}
+              />
+              <FormErrorMessage>
+                {errors.phone}
+              </FormErrorMessage>
+            </FormControl>
+            
+            <FormControl isInvalid={errors.description}>
+              <FormLabel htmlFor='description'>Assunto</FormLabel>
+              <Input
+                id='description'
+                placeholder='Assunto'
+                {...register("description")}
+                onChange={(event) => {
+                  setSubject(event.target.value)
+                }}
+              />
+              <FormErrorMessage>
+                {errors.description}
+              </FormErrorMessage>
+            </FormControl>
+            
+            <Button mt={4} colorScheme='teal' isLoading={isSubmitting} type='submit'>
+              Enviar
+            </Button>
+          </VStack>
         </form>
       )
 }
